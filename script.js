475b7da4d59e0140686436dd21ac423832063e2f
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2022-05-31 21:58:54 +0530

    button based dark mode enable: working!

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-$( ".inner-switch" ).on("click", function() {
-  if( $( "body" ).hasClass( "dark" )) {
-    $( "body" ).removeClass( "dark" );
-    $( ".inner-switch" ).text( "OFF" );
-  } else {
-    $( "body" ).addClass( "dark" );
-    $( ".inner-switch" ).text( "ON" );
-  }
-});
+document.addEventListener('DOMContentLoaded', () => {
+
+  const themeStylesheet = document.getElementById('theme');
+  const themeToggle = document.getElementById('theme-toggle');
+  themeToggle.addEventListener('click', () => {
+      // if it's light -> go dark
+      if(themeStylesheet.href.includes('light')){
+          themeStylesheet.href = 'dark-theme.css';
+          themeToggle.innerText = 'Switch to light mode';
+      } else {
+          // if it's dark -> go light
+          themeStylesheet.href = 'light-theme.css';
+          themeToggle.innerText = 'Switch to dark mode';
+
+      }
+  })
+})
