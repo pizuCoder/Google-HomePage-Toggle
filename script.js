e2d1cfa7047c5004fa785f614cdfd813e127f0c0
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2022-06-01 13:42:48 +0530

    changed colours in dark theme

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ document.addEventListener('DOMContentLoaded', () => {
       // if it's light -> go dark
       if(themeStylesheet.href.includes('light')){
           themeStylesheet.href = 'dark-theme.css';
-          themeToggle.innerText = 'Switch to light mode';
+          themeToggle.innerText = 'light mode';
+          
       } else {
           // if it's dark -> go light
           themeStylesheet.href = 'light-theme.css';
-          themeToggle.innerText = 'Switch to dark mode';
-
+          themeToggle.innerText = 'dark mode';
+          
       }
   })
 })
+
+
+    
