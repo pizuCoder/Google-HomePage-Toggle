8a11c7addf675bb5fcd5fc105714ad1a00f79aad
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2022-06-07 16:37:06 +0530

    dark theme changes

--- html&css ONLY/script.js
+++ html&css ONLY/script.js
@@ -7,12 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
       if(themeStylesheet.href.includes('light')){
           themeStylesheet.href = 'dark-theme.css';
           themeToggle.innerText = 'light mode';
+          document.getElementById('logo').src = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_light_color_272x92dp.png";
           
       } else {
           // if it's dark -> go light
           themeStylesheet.href = 'light-theme.css';
           themeToggle.innerText = 'dark mode';
-          
+          document.getElementById('logo').src = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
       }
   })
 })
